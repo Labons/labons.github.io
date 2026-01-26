b63ce91682a6d48081a9d920b82a2b45d87daf08
bvance <labongenshin@gmail.com>
2026-01-26 18:03:27 +0800

    Removed everything

--- script.js
+++
@@ -1,5 +0,0 @@
-// Add event listener to learn more button
-document.querySelector('#home button').addEventListener('click', () => {
-    // Scroll to about section
-    document.querySelector('#about').scrollIntoView({ behavior: 'smooth' });
-});
